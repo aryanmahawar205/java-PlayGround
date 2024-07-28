d6b8841fd38117e832ea445f983a3d132a53cb13
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-07-28 15:53:41 +0530

    basics in java

---
+++ JavaBasics/src/HelloWorld.java
@@ -0,0 +1,14 @@
+//package com.company;
+// a package in Java is used to group related classes.
+// think of it as a folder in a file directory
+
+// in public static void main, the keyword static means
+// that it makes it possible for us to run this function of the class helloWorld without making any objects of the class
+
+public class HelloWorld
+{
+    public static void main (String args[])
+    {
+        System.out.println("Hello World!");
+    }
+}
