0b785b4cee38a5412947986347934b43c89c13fa
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-09-15 16:51:44 +0530

    dir loc change

---
+++ JavaObjectOrientedProgramming/src/Encapsulation in Java/Packages/Information.java
@@ -0,0 +1,10 @@
+import bank.*;
+
+public class Information
+{
+    public static void main(String[] args)
+    {
+        bank.Account account1 = new bank.Account();
+        account1.name = "Customer1";
+    }
+}
