5b5b3f7a1154f7407f005826e495489693daf034
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-09-20 22:25:17 +0530

    add @Override annotations

--- DetailedJavaOOPS/src/Polymorphism/Circle.java
+++ DetailedJavaOOPS/src/Polymorphism/Circle.java
@@ -2,6 +2,8 @@ package Polymorphism;
 
 class Circle extends Shapes
 {
+    // annotation added for just to check overriding
+    @Override
     void area()
     {
         System.out.println("Area function in Circle class");
--- DetailedJavaOOPS/src/Polymorphism/Square.java
+++ DetailedJavaOOPS/src/Polymorphism/Square.java
@@ -2,6 +2,8 @@ package Polymorphism;
 
 class Square extends Shapes
 {
+    // annotation added for just to check overriding
+    @Override
     void area()
     {
         System.out.println("Area function in Square class");
