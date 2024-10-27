4a2cfdcaec30a6c4eaa77223058e190a81ac2ce7
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-10-27 15:40:07 +0530

    File Reader

---
+++ JavaFileHandling/src/Streams/FileReader/Main.java
@@ -0,0 +1,28 @@
+package Streams.FileReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            FileReader fr = new FileReader("D:\\java-PlayGround\\JavaFileHandling\\src\\Streams\\FileReader\\note.txt");
+            int letters;
+
+            while ((letters = fr.read()) != -1)
+            {
+                System.out.println((char) letters);
+            }
+
+            fr.close();
+        }
+
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
